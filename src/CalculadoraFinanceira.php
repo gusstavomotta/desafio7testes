d795523244c14eb9885adb0d58e081658cfd9743
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-02-21 11:52:33 -0300

    Atualizado 11:52

--- src/CalculadoraFinanceira.php
+++ src/CalculadoraFinanceira.php
@@ -3,14 +3,23 @@
 class CalculadoraFinanceira{
     public const tipos_amortizacao = ['sac', 'price'];
 
-    public function calcularJurosSimples($capital, $taxa, $tempo){
-        
-    }
+    // public function calcularJurosSimples($capital, $taxa, $tempo){
+    //     return  $capital * ($taxa/100 + 1) * $tempo;
+    // }
     public function calcularJurosCompostos($capital, $taxa, $tempo){
-        
-    }
 
-    public function calcularAmortizacao($capital, $taxa, $tempo,$tipo){
+        if ($tempo == 0){
+            return number_format($capital, 2, ",", "");
+        }
+        
+        $capital = ($capital * ($taxa/100 + 1));
+        $tempo --;
+        return $this->calcularJurosCompostos($capital, $taxa, $tempo);
+    }
+    // public function calcularJurosCompostos2($capital, $taxa, $tempo){
+    //   return $capital * pow(($taxa/100 + 1), $tempo);
+    // }
+    public function calcularAmortizacao($capital, $taxa, $tempo,$tipo,$saldoDevedor){
 
     }
 }
--- tests/CalculadoraFinaceiraTest.php
+++ tests/CalculadoraFinaceiraTest.php
@@ -1,5 +1,8 @@
 <?php 
 use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
 require_once "src/CalculadoraFinanceira.php";
 
 class CalculadoraFinaceiraTest extends TestCase {
@@ -10,25 +13,28 @@ class CalculadoraFinaceiraTest extends TestCase {
         $this->calculadoraFinanceira = new CalculadoraFinanceira;
     }
 
-    public function testCalcularJurosSimples(){
-        $capital = 1000;
-        $taxa = 0.5;
-        $tempo = 2;
-        $this->calculadoraFinanceira->calcularJurosSimples($capital, $taxa, $tempo);
-    }
+    // public function testCalcularJurosSimples(){
+    //     $capital = 1000;
+    //     $taxa = 0.5;
+    //     $tempo = 2;
+
+    //     $juros = $this->calculadoraFinanceira->calcularJurosSimples($capital, $taxa, $tempo);
+    //     $this->assertEquals(1100, $juros);
+    //     }
     public function testCalcularJurosCompostos(){
         $capital = 1000;
-        $taxa = 0.5;
-        $tempo = 2;
-        $this->calculadoraFinanceira->calcularJurosCompostos($capital, $taxa, $tempo);
+        $taxa = 10;
+        $tempo = 3;
 
+        $montante = $this->calculadoraFinanceira->calcularJurosCompostos($capital, $taxa, $tempo);
+        $this->assertEquals('1331,00', $montante);
     }
-    public function testCalcularAmortizacao(){
-        $capital = 1000;
-        $taxa = 0.5;
-        $tempo = 2;
-        $tipo = 'sac';
-        $this->calculadoraFinanceira->calcularAmortizacao($capital, $taxa, $tempo,$tipo);
+    // public function testCalcularAmortizacao(){
+    //     $capital = 1000;
+    //     $taxa = 0.5;
+    //     $tempo = 2;
+    //     $tipo = 'sac';
+    //     $this->calculadoraFinanceira->calcularAmortizacao($capital, $taxa, $tempo,$tipo);
 
-    }
+    // }
 }
