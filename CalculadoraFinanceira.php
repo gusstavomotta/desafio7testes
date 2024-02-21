58c64b0c42e838c1b78cb670dc2aabf49f836f9d
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-02-21 10:46:35 -0300

    CriaçãoClasse

---
+++ CalculadoraFinanceira.php
@@ -0,0 +1,16 @@
+<?php 
+
+class CalculadoraFinanceira{
+    public const tipos_amortizacao = ['sac', 'price'];
+
+    public function calcularJurosSimples($capital, $taxa, $tempo){
+        
+    }
+    public function calcularJurosCompostos($capital, $taxa, $tempo){
+        
+    }
+
+    public function calcularJurosAmortizacao($capital, $taxa, $tempo,$tipo){
+
+    }
+}
